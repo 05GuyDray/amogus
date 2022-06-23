87434837e04c0867905d6b3b5a2962e86c3a5289
unknown <nadav8.tch05@gmail.com>
2022-06-23 12:21:18 +0300

    amogus

--- bash.bat
+++ bash.bat
@@ -1,318 +1,240 @@
 
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
 git add .
 git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-
-
-echo amogus>> amogus.txt
-git add .
-git commit -m "amogus"
-git push
-echo Amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
+git add .
+git commit -m "amogus"
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
-echo Amogus>> amogus.txt
+
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
+git add .
+git commit -m "amogus"
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
 
-
-echo amogus>> amogus.txt
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
+git add .
+git commit -m "amogus"
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
-echo Amogus>> amogus.txt
+
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
+git add .
+git commit -m "amogus"
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
+git add .
+git commit -m "amogus"
+git push
+
+del amogus.txt
+type nul> amogus.txt
+echo amogus> amogus.txt
+git add .
+git commit -m "amogus"
+del amogus.txt
+type nul> amogus.txt
+echo Amogus> amogus.txt
 git add .
 git commit -m "amogus"
 git push
